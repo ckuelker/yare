96519a7dc743af2674017ac016b1b887a56c1310
Christian Kuelker (s1) <c@c8i.org>
2023-03-09 08:13:49 +0100

    Improve writing, formatting, Add boiler plate

--- lib/Yare.pm
+++ lib/Yare.pm
@@ -1,92 +1,119 @@
+# +---------------------------------------------------------------------------+
+# | Yare                                                                      |
+# |                                                                           |
+# | Manual.                                                                   |
+# |                                                                           |
+# | Version: 0.0.6 (change inline)                                            |
+# |                                                                           |
+# | Changes:                                                                  |
+# |                                                                           |
+# | 0.0.6 2023-03-09 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Add boiler plate                                                    |
+# |     - Improve writing                                                     |
+# |     - Improve formatting                                                  |
+# | 0.0.5 2014-08-22 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Explanation of YARE_CFG feature                                     |
+# | 0.0.4 2013-08-06 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Fix further information (self reference)                            |
+# | 0.0.3 2023-07-23 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Add idea "yatta!"                                                   |
+# | 0.0.2 2013-03-10 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Add headings for POD                                                |
+# |     - Improve POD                                                         |
+# | 0.0.1 2013-03-09 Christian Külker <christian.kuelker@cipworx.org>         |
+# |     - Initial release                                                     |
+# |                                                                           |
+# +---------------------------------------------------------------------------+
+
 package Yare;
 
-# ABSTRACT: yare - do it! Distributed todo prove of concept
+# ABSTRACT: Yare - do it! Distributed todo prove of concept.
 
-our $VERSION = '0.0.5';
+our $VERSION = '0.0.6';
 
 1;
 __END__
 
 =pod
 
-=head1 NAME 
+=head1 NAME
 
-yare - do it! Distributed todo prove of concept
+Yare - do it! Distributed todo prove of concept.
 
 =head1 MOTIVATION
 
-Often I experienced that data gets lost, when using calendar or todo lists
-shared over a central daemon. That is quite disappointing. Other software
-claims that its easy to share and to synchronize them, because they are just
-using one file. I doubt that there is a safe way for sharing one file over
-three laptops (for example), if in addition one consider that laptops are not
-always online. Even Git - which handles merges quite good - is not a solution
-to this problem. It is very likely that the unexperienced or busy user ends up
-in front of a complex merge conflict question. 
+I have often seen data lost when using calendar or todo lists shared via a
+central daemon. This is quite disappointing. Other software claims to be easy
+to share and synchronize because they only use one file. I doubt that there is
+a secure way to share a file across (say) three laptops, considering that
+laptops are not always online. Even Git - which handles merges quite well - is
+no solution to this problem. It is very likely that the inexperienced or busy
+user will end up with a complex merge conflict.
 
 =head1 TARGET
 
-Synchronize todo list information among 3 clients, which can be online or
-offline without merge conflicts or loss of (non unique!) data.
+Synchronize todo list information between 3 clients, which can be online or
+offline, without merge conflicts or loss of (non-unique!) data.
 
 =head1 THESIS
 
-a) The loss of non unique (doubled) data can be tolerated by the user.
-b) Conflicts rose by duplicated data can be solved easily by the user.
-   (by deleting unwanted entries)
+a) The loss of non-unique (duplicate) data can be tolerated by the user.
+
+b) Conflicts caused by duplicate data can be easily resolved by the user. (by
+deleting unwanted entries)
 
 =head1 SOLUTION
 
-(1) A git repository as central storage and share facility. 
+(1) A git repository for centralized storage and sharing.
 
-(2) Every todo entry has a time stamp when it was created
+(2) Each todo item has a timestamp indicating when it was created.
 
-(3) Every todo entry has a time stamp when it was marked as 'yatta!' (done)
+(3) Each todo entry has a timestamp of when it was marked as "yatta!" (done).
 
-(4) Every add, remove and commit can be done also offline
+(4) Every 'add', 'remove' and 'commit' can be done also offline.
 
-(5) When Internet connection is restored the information will be shared among
-    all clients.
+(5) When the Internet connection is restored, the information is shared among
+all clients.
 
-(6) Every todo entry is stored in a separate file and the name of the file is
-    the md5 sum of its content.
+(6) Each todo entry is stored in a separate file, and the name of the file is
+the md5 sum of its contents.
 
 =head1 DISCUSSION
 
-As it is obvious from point (2), (3) and (6) a git merge conflict is seldom.
-However not impossible. It is possible if computer A create the same todo text
-as computer B at exactly the same second for example. If then both computers
-will sync their todo list at the same time, only one git push/pull will
-succeed. The other will be rejected. 
+As you can see from points (2), (3) and (6), a git merge conflict is rare.
+However, it is not impossible. It is possible, for example, if computer A
+creates the same todo text as computer B at exactly the same second. If both
+computers synchronize their todo lists at the same time, only one git push/pull
+will succeed. The other will fail.
 
-This sounds like a problem, but in reality it is not. The only thing which was
-rejected was a binary identical information in a binary identical file name. A
-second push/pull will not be rejected. 
+This sounds like a problem, but it is not. The only thing that was rejected was
+binary identical information in a binary identical filename. A second push/pull
+is not rejected.
 
-This was experimental proven with 2 laptops. 
+This has been verified experimentally with 2 laptops.
 
-Regarding point b). The user can easily remove doubled entries.
+Regarding the second point: The user can easily remove duplicate entries.
 
-I would like to hear more opinions about this concept and of course I am
-interested in, if there is a flaw in this concept.
+I would like to hear more opinions about this concept, and of course I am
+interested if there is a flaw in this concept.
 
 =head1 USAGE
 
-Execute the script
+Run the script
 
    yare
 
-If you enter 'help'<RETRUN> you get a brief list of available commands.  Use
-'add' to create an entry. Add is used like:
+If you type 'help'<RETRUN> you will get a short list of available commands.
+Use 'add' to create an entry. Add is used like:
 
     add Something I need to do
 
 Use 'fetch' to display the list:
 
     fetch
-    ---------------------------------------------------------------------------- 
+    ----------------------------------------------------------------------------
     0: Something I need to do
-    ---------------------------------------------------------------------------- 
+    ----------------------------------------------------------------------------
 
 To remove this entry, you can mark it as 'yatta!' (done) like:
 
@@ -98,8 +125,8 @@ Yare will then print
 
 ## ADVANCED USAGE
 
-It is possible to use more then one yare todo list. The example show how to
-create a second todo list for work and how to use it
+It is possible to use more than one todo list. The example shows how to create
+a second todo list for work and how to use it.
 
     mkdir -p ~/.yare/work
     echo "[online]" > ~/.yare/work/yare.ini
@@ -107,15 +134,15 @@ create a second todo list for work and how to use it
     echo "[git]" >> ~/.yare/work/yare.ini
     echo "worktree=~/.yare/work/todo-work" ~/.yare/work/yare.ini
 
-You have to create a remote git repository 'todo-work' and clone it to
-~/.yare/work/todo-work. Then you can use it like:
+You need to create a remote git repository called 'todo-work' and clone it to
+~/.yare/work/todo-work. Then you can use it as usual:
 
     export YARE_CFG=~/.yare/work
     yare
 
 ## FURTHER INFORMATION
 
-You can get further information via the ordinary Perl documentation.
+For more information, see the standard Perl documentation.
 
     perldoc Yare::Client
 
@@ -129,7 +156,7 @@ L<Yare::Client> - the current implementation
 
 =head1 AUTHOR
 
-    Christian Külker <christian.kuelker@cipworx.org> 
+    Christian Külker <christian.kuelker@cipworx.org>
 
 =cut
 
